63d3131498105d58c4cbac01311e00836471d5aa
Listiarso Wastuargo <lwastuargo@Listiarsos-MacBook-Pro.local>
2021-07-24 10:32:48 +0700

    Implement the actual Add API

--- src/pages/api/add.js
+++ src/pages/api/add.js
@@ -1,4 +1,8 @@
 module.exports = (req, res) => {
-    const { name = 'World' } = req.query;
-    res.status(200).send(`Hello ${name}!`);
+    const { num1 = 0, num2 = 0 } = req.query;
+    const num1Int = parseInt(num1) || 0;
+    const num2Int = parseInt(num2) || 0;
+    res.status(200).send({
+        result: num1Int + num2Int,
+    });
 };
